1e8b83ee15ddd0b725b80607b315241d9938a039
Tony Cavella <tony@cavella.com>
2019-05-03 20:05:49 -0400

    Added URL encoding to variables

--- rdconfig.ps1
+++ rdconfig.ps1
@@ -26,10 +26,18 @@ function testoutput {
 # Script Logic
 foreach($line in Get-Content $sourceFile) {
     Write-Host $line
-    $ssid,$guipass,$pskpass = $line.split(' ')
+    $ssid,$devpin,$guipass,$pskpass = $line.split(' ')
+    Copy-Item "$__confDir\default.conf" -Destination "$__outDir\$ssid.conf"
     Write-Host $ssid
+    Write-Host $devpin
+    $devpinENC = [System.Web.HttpUtility]::UrlEncode($devpin)
+    Write-Host $devpinENC
     Write-Host $guipass
+    $guipassENC = [System.Web.HttpUtility]::UrlEncode($guipass)
+    Write-Host $guipassENC
     Write-Host $pskpass
+    $pskpassENC = [System.Web.HttpUtility]::UrlEncode($pskpass)
+    Write-Host $pskpassENC
 }
 
 exit 0
